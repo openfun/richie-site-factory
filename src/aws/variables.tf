79f5ecba762b79bf9eda79caa41216c12abbad9f
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2020-05-26 18:46:08 +0200

    ♻️(aws) remove now useless static files bucket
    Files are now stored in a custom nginx image so we don't need
    the AWS S3 bucket for that. The Cloudfront origin for static
    files should now point to our nginx.

--- src/aws/variables.tf
+++ src/aws/variables.tf
@@ -11,3 +11,13 @@ variable "cloudfront_price_class" {
     production = "PriceClass_All"
   }
 }
+
+variable "app_domain" {
+  type = "map"
+
+  default = {
+    production = "www.fun-mooc.fr"
+    preprod = "preprod.fun.oc.openfun.fr"
+    staging = "staging.fun.oc.openfun.fr"
+  }
+}
