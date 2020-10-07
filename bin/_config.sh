614cfb899405ba3b87dcd4c2149acd57bd2d466d
Samuel Paccoud - FUN MOOC <samuel.paccoud@fun-mooc.fr>
2020-10-07 22:26:06 +0200

    ✨(project) automate richie upgrades and sites releases
    Upgrading all sites to a new richie version and making site
    releases is a very repetitive and time consumming task. We
    should automate them.

--- bin/_config.sh
+++ bin/_config.sh
@@ -6,6 +6,12 @@ SITES_DIRECTORY="sites"
 # Set default active site
 RICHIE_SITE="${RICHIE_SITE:-funmooc}"
 
+# console colors
+declare -r COLOR_INFO="\033[0;36m"
+declare -r COLOR_RESET="\033[0m"
+
+export COLOR_INFO
+export COLOR_RESET
 export PROJECT_DIRECTORY
 export SITES_DIRECTORY
 export RICHIE_SITE
