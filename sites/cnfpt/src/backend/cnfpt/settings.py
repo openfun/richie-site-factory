920573611e607c5146fa4b39f154cb9505bd3d17
jbpenrath <jb.penrath@gmail.com>
2020-11-30 09:19:04 +0100

    ⬆️(cnfpt) upgrade richie to v2.0.0-beta.21
    Changelog available at:
    https://github.com/openfun/richie/releases/tag/v2.0.0-beta.21

--- sites/cnfpt/src/backend/cnfpt/settings.py
+++ sites/cnfpt/src/backend/cnfpt/settings.py
@@ -198,7 +198,7 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
             "", environ_name="AUTHENTICATION_BASE_URL", environ_prefix=None
         ),
         "BACKEND": values.Value(
-            "richie.apps.courses.lms.base.BaseLMSBackend",
+            "base",
             environ_name="AUTHENTICATION_BACKEND",
             environ_prefix=None,
         ),
@@ -223,7 +223,12 @@ class Base(StyleguideMixin, DRFMixin, RichieCoursesConfigurationMixin, Configura
             "BACKEND": values.Value(
                 "richie.apps.courses.lms.base.BaseLMSBackend",
                 environ_name="EDX_BACKEND",
-                environ_prefix=None
+                environ_prefix=None,
+            ),
+            "JS_BACKEND": values.Value(
+                "base",
+                environ_name="EDX_JS_BACKEND",
+                environ_prefix=None,
             ),
             "SELECTOR_REGEX": values.Value(
                 r".*", environ_name="EDX_SELECTOR_REGEX", environ_prefix=None
